6ada04e2461252b9bf6a78c18fd904866f997c3e
unknown <pjfortuna2306@gmail.com>
2023-05-30 22:12:08 +0800

    Added a copyright and message below navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,20 @@
 import "./NavBar.css";
 import myLogo from "../../public/prince_logo.png";
 import { useState, useEffect } from "react";
+import { AiOutlineCopyright } from "react-icons/ai";
 const NavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [ismenuFinishedOpenning, setIsMenuFinishedOpenning] = useState(false);
+  const [isMenuFinishedDelayDone, setIsMenuFinishedDelayDone] = useState(false);
 
   const clickMenuHandler = () => {
     setIsMenuOpen((prevState) => !prevState);
 
     setTimeout(() => {
       setIsMenuFinishedOpenning((prevState) => !prevState);
+      setTimeout(() => {
+        setIsMenuFinishedDelayDone((prevState) => !prevState);
+      }, 1000);
     }, 500);
   };
   const lineStyle = {
@@ -64,13 +69,11 @@ const NavBar = () => {
           >
             <svg width={43} height={43} viewBox="0 0 100 100">
               <path
-                
                 className="line line1"
                 d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
               />
               <path className="line line2" d="M 20,50 H 80" />
               <path
-                
                 className="line line3"
                 d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
               />
@@ -114,24 +117,76 @@ const NavBar = () => {
           <div
             className={`menu-titles font-poppins   text-center  space-y-12 text-4xl sm:text-3xl`}
           >
-            <div className={`opacity-60 font-normal hover:font-medium hover:animate-pulse hover:opacity-60 ${isMenuOpen ? "about" : ""}`}>
+            <div
+              className={`opacity-30 font-normal hover:font-medium hover:animate-pulse hover:opacity-60 ${
+                isMenuOpen ? "about" : ""
+              }`}
+            >
               <span className="">{ismenuFinishedOpenning ? "about" : ""}</span>
             </div>
-            <div className={` opacity-60 font-normal hover:font-medium hover:animate-pulse hover:opacity-60 ${isMenuOpen ? "projects" : ""}`}>
+            <div
+              className={` opacity-30 font-normal hover:font-medium hover:animate-pulse hover:opacity-60 ${
+                isMenuOpen ? "projects" : ""
+              }`}
+            >
               <span>{ismenuFinishedOpenning ? "projects" : ""}</span>
             </div>
-            <div className={`opacity-60 font-normal hover:font-medium hover:animate-pulse hover:opacity-60 ${isMenuOpen ? "experience" : ""}`}>
+            <div
+              className={`opacity-30 font-normal hover:font-medium hover:animate-pulse hover:opacity-60 ${
+                isMenuOpen ? "experience" : ""
+              }`}
+            >
               <span>{ismenuFinishedOpenning ? "experience" : ""}</span>
             </div>
-            <div className={`opacity-60 font-normal hover:font-medium hover:animate-pulse hover:opacity-60 ${isMenuOpen ? "contact" : ""}`}>
+            <div
+              className={`opacity-30 font-normal hover:font-medium hover:animate-pulse hover:opacity-60 ${
+                isMenuOpen ? "contact" : ""
+              }`}
+            >
               <span>{ismenuFinishedOpenning ? "contact" : ""}</span>
             </div>
-            <div className={`pb-1 opacity-60 font-normal hover:font-medium hover:animate-pulse hover:opacity-60 ${isMenuOpen ? "blogs" : ""}`}>
+            <div
+              className={`pb-1 opacity-30 font-normal hover:font-medium hover:animate-pulse hover:opacity-60 ${
+                isMenuOpen ? "blogs" : ""
+              }`}
+            >
               <span>{ismenuFinishedOpenning ? "blogs" : ""}</span>
             </div>
           </div>
         </div>
       </div>
+
+      <div
+        className={`${
+          isMenuOpen ? "" : "hidden "
+        }copyright text-orange-500 bottom-5 absolute text-center right-0 left-0`}
+      >
+        <div
+          className={`${
+            ismenuFinishedOpenning ? "c-message" : ""
+          } font-thin flex flex-col text-sm ${ismenuFinishedOpenning?"opacity-100":"opacity-0"}`}
+        >
+          {ismenuFinishedOpenning ? (
+            <>
+              <span>
+                Designed and Made with{" "}
+                <span className="text-orange-400">Passion</span> and{" "}
+                <span className="text-orange-400">Love</span>
+              </span>
+              <span>
+                Prince Jairo Fortuna{" "}
+                <div
+                  style={{ display: "inline-block", verticalAlign: "middle" }}
+                >
+                  <AiOutlineCopyright />
+                </div>
+                2022
+              </span>
+            </>
+          ) : null}
+        </div>
+      </div>
+
       {/*menu links */}
     </>
   );
